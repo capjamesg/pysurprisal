28536d356f82f79a5a2e262612a6f108e506078d
James <37276661+capjamesg@users.noreply.github.com>
2023-12-15 12:25:27 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/capjamesg/pysurprisal",
     install_requires=[
-        "scipy.special"
+        "scipy"
     ],
     packages=find_packages(exclude=("tests",)),
     extras_require={
